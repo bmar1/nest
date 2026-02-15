23f53d00cfeff83b9aecb6f0822d17700acd2100
bmar1 <bilalutwo@gmail.com>
2026-02-14 19:48:12 -0500

    Inital setup and commit

---
+++ src/main/java/com/nest/nestapp/NestappApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/nestapp/NestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.nestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
